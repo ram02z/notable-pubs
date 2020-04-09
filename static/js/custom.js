1744f5f18186cbf925589b776472d19598fd8459
Omar Zeghouani <omarzeghouanii@gmail.com>
2020-04-09 21:37:48 +0100

    changed

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,20 +1,2 @@
-$(document).ready(function () {
-  $('#heroes, #players').change(function () {
-    select1 = $('#heroes').val();
-    select2 = $('#players').val();
-    if (select1.length == 0 || select2.length == 0) {
-        $('#submit').prop("disabled", true);
-    }
-    else {
-        $('#submit').prop("disabled", false);
-    }
-  })
-});
-$(document).ready( function () {
-    $('#winloss').DataTable({
-        "paging": false,
-        "searching": false,
-        "stateSave": true,
-        "stateDuration": 0
-    });
-} );
+$(document).ready(function(){$('#heroes, #players').change(function(){select1=$('#heroes').val();select2=$('#players').val();if(select1.length==0||select2.length==0){$('#submit').prop("disabled",!0)}
+else{$('#submit').prop("disabled",!1)}})});$(document).ready(function(){$('#winloss').DataTable({"paging":!0,"pageLength":5,"lengthMenu":[[5,10,-1],[5,10,"All"]],"searching":!1,"stateSave":!0,"stateDuration":0})})
